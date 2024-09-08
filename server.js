a1478bb0565d06b18d8028930aa3eebfb0cffda4
sabri-manai <sabri.manai@medtech.tn>
2024-09-08 12:42:43 +0200

    to deploy on heroku

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.use(cors());
 
 AWS.config.update({
   region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.MY_ACCESS_KEY_ID,
+  secretAccessKey: process.env.MY_SECRET_ACCESS_KEY,
 });
+
 
 const cognito = new AWS.CognitoIdentityServiceProvider();
 
